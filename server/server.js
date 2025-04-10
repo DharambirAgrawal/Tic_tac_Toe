8522a5c9018d1ccafb078031a5ffcd3adef33e0f
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-04-10 23:01:41 +0000

    Updated reame file

--- server/server.js
+++ server/server.js
@@ -1,32 +1,3 @@
-// // app.js
-// import express from 'express';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-// import { dirname } from 'path';
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // __dirname workaround for ES modules
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// // Serve static files from the public directory
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Serve the game page
-// app.get('/bot', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/bot', 'bot.html'));
-// });
-
-// app.get('/online', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/online', 'index.html'));
-// });
-
-// // Start the server
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
